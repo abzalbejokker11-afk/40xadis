14c8d8143ff1ac8c658b7ad7393044fe651d239d
abzalbejokker11-afk <abzalbejokker11@gmail.com>
2026-07-18 23:32:18 +0500

    Til tuzatish: lotin rejimida savollar lotinda (translit), kesh v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'qirqhadis-v7';
+var CACHE = 'qirqhadis-v8';
 self.addEventListener('install', function(e){
   e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(['./', './index.html', './manifest.webmanifest', './icon-192.png']); }).then(function(){ return self.skipWaiting(); }));
 });
